51960f13ed6064489086a0a68fab62d3d2c766ab
ibrhmusta <ibrhmusta99@gmail.com>
2021-01-29 22:28:54 +0300

    MyDictionary Work

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, E>
+
+    {
+        T[] _arrValue;
+        E[] _arrNumber;
+
+        public MyDictionary()
+        {
+            _arrValue = new T[0];
+            _arrNumber = new E[0];
+        }
+
+        public void Add(T item1, E item2)
+        {
+            T[] _tempValue = _arrValue;
+            E[] _tempNumber = _arrNumber;
+
+            _arrValue = new T[_arrValue.Length + 1];
+            _arrNumber = new E[_arrNumber.Length + 1];
+
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _arrValue[i] = _tempValue[i];
+                _arrValue[_arrValue.Length - 1] = item1;
+            }
+
+            for (int i = 0; i < _tempNumber.Length; i++)
+            {
+                _arrNumber[i] = _tempNumber[i];
+                _arrNumber[_arrNumber.Length - 1] = item2;
+            }
+            Console.WriteLine("|" + item1 + "|" + " Şehrinin Plakası" + " " + "--->" + " " + "|" + item2 + "|");
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> IlPlakalar = new MyDictionary<string, int>();
+
+            IlPlakalar.Add("ERZURUM", 25);
+            IlPlakalar.Add("ADANA", 01);
+            IlPlakalar.Add("YALOVA", 77);
+            IlPlakalar.Add("İZMİR", 35);
+            IlPlakalar.Add("İSTANBUL", 34);
+            IlPlakalar.Add("BALIKESİR", 10);
+            IlPlakalar.Add("ERZİNCAN", 24);
+            IlPlakalar.Add("DÜZCE", 81);
+            IlPlakalar.Add("ANKARA", 06);
+            IlPlakalar.Add("BURSA", 16);
+            IlPlakalar.Add("OSMANİYE", 80);
+            IlPlakalar.Add("KARABÜK", 78);
+            IlPlakalar.Add("KİLİS", 79);
+
+
+
+
+        }
+    }
+}
